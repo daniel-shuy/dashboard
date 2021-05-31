d9ad6046249502498083bff53ecbd513e1b6a81f
shivani170 <shivani@devtron.ai>
2021-06-01 00:01:43 +0530

    test cases of filters done

--- src/components/app/list/__test__/Applist.test.tsx
+++ src/components/app/list/__test__/Applist.test.tsx
@@ -4,7 +4,7 @@ import renderer from 'react-test-renderer';
 import ExternalListContainer from '../ExternalListContainer';
 import AppListContainer from '../AppListContainer';
 import { Filter } from '../../../common';
-import { render, cleanup, fireEvent, screen, queryByPlaceholderText } from '@testing-library/react';
+import { render, cleanup, fireEvent, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 
 afterEach(() => cleanup());
@@ -18,7 +18,7 @@ it('applist renders without crashing matches snapshot', () => {
     expect(appList).toMatchSnapshot();
 });
 
-it("render search correctly", ()=>{
+it("render search correctly", () => {
     const { queryByPlaceholderText } = render(<BrowserRouter><AppListContainer /></BrowserRouter>)
     expect(queryByPlaceholderText("Search apps")).toBeTruthy()
 })
@@ -30,16 +30,16 @@ it("should render list", () => {
 });
 
 describe("input value", () => {
-  it("update search while changing value", () => {
-    const { queryByPlaceholderText } = render(<BrowserRouter><AppListContainer /></BrowserRouter>)
-    const searchInput = queryByPlaceholderText("Search apps")
-    fireEvent.change(searchInput, {
-        target: {
-            value: "testing"
-        }
+    it("update search while changing value", () => {
+        const { queryByPlaceholderText } = render(<BrowserRouter><AppListContainer /></BrowserRouter>)
+        const searchInput = queryByPlaceholderText("Search apps")
+        fireEvent.change(searchInput, {
+            target: {
+                value: "testing"
+            }
+        })
+        expect(searchInput["value"]).toBe("testing")
     })
-    expect(searchInput["value"]).toBe("testing")
-  })
 })
 
 it('render filter successfull', () => {
@@ -60,34 +60,55 @@ it('render filter successfull', () => {
 })
 
 
-it('render app list successfull: apply filter env', () => {
+it('render app list successfull: apply filter in environment', () => {
 
     const mockApplyFilter = jest.fn();
     const { queryByTestId } = render(
-        <BrowserRouter>
-            <AppListContainer />
-        </BrowserRouter>
+        <Filter list={[{ key: 1, label: 'devtron', isSaved: false, isChecked: false }]}
+            labelKey="label"
+            buttonText="Environment: "
+            searchable multi
+            placeholder="Search Environment"
+            type={"environment"}
+            applyFilter={mockApplyFilter}
+        />
     );
     let btn = queryByTestId('apply-filter-environment')
     fireEvent.click(btn)
-    // expect(mockApplyFilter).toHaveBeenCalled()
+    expect(mockApplyFilter).toHaveBeenCalled()
 })
 
-it('render app list successfull: apply status filter ', () => {
+it('render app list successfull: apply filter in status', () => {
     const mockApplyFilter = jest.fn();
     const { queryByTestId } = render(
-        <BrowserRouter>
-            <AppListContainer />
-        </BrowserRouter>
+        <Filter list={[{ key: 1, label: 'devtron', isSaved: false, isChecked: false }]}
+        labelKey="label"
+        buttonText="Status: "
+        searchable multi
+        placeholder="Status: All"
+        type={"status"}
+        applyFilter={mockApplyFilter}
+    />
     );
     let btn = queryByTestId('apply-filter-status')
-    // fireEvent.click(btn)
-    // expect(mockApplyFilter).toBeCalled()
+    fireEvent.click(btn)
+    expect(mockApplyFilter).toHaveBeenCalled()
 })
 
-it('render app list successfull: apply status filter ', () => {
+it('render app list successfull: apply filter in project', () => {
     const mockApplyFilter = jest.fn();
-    const { getByTestId } = render(<BrowserRouter><AppListContainer /></BrowserRouter>);
-    // let statusEl = getByTestId('environment-filter')
-    expect(mockApplyFilter).not.toHaveBeenCalled();
+    const { queryByTestId } = render(
+        <Filter list={[{ key: 1, label: 'devtron', isSaved: false, isChecked: false }]}
+        labelKey="label"
+        buttonText="Project: "
+        searchable multi
+        placeholder="Project: All"
+        type={"team"}
+        applyFilter={mockApplyFilter}
+    />
+    );
+    let btn = queryByTestId('apply-filter-team')
+    fireEvent.click(btn)
+    expect(mockApplyFilter).toHaveBeenCalled()
 })
+
